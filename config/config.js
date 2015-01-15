57b36dd8c29d93842b1c86d8240a1ec98cd0f5ce
Greg Milligan <xybersolve@gmail.com>
2015-01-15 15:54:33 -0500

    refactor project structure

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "mongo": {
+    "username": "username",
+    "password": "password",
+    "database": "DatabaseName",
+    "host"    : "localhost",
+    "port"    : "27017"
+  }
+};
---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+
+ A bare bones example of getting mongoWrap up and running
+
+ -- If you have questions or feedback: xybersolve@gmail.com
+
+*/
+
+var cfg              = require('./config/config')
+  , mongoWrap        = require('./src/mongo-wrap')(cfg.mongo);
+
+mongoWrap.connect(function(err, db) {  "use strict";
+  if(err) throw err;
+  runAfterDBConnect();
+});
+
+function runAfterDBConnect(){
+  var query = {
+    collection: 'myCollection',
+    where: {myField: 'myValue'},
+    sort: {mySortField: 1}
+  };
+  mongoWrap.findAll(query, function(err, results){
+    if(err) return cb(err);
+    if(results){
+      // do something with results
+    }
+  });
+}
